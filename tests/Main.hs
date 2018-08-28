e10208cf862ee5aded8f42e9c27a9651f3926fd4
Taylor Fausak <taylor@fausak.me>
2018-08-28 19:09:51 -0400

    Count allocations when testing

--- tests/Main.hs
+++ tests/Main.hs
@@ -3,7 +3,28 @@ module Main
   )
 where
 
+import qualified Control.Exception as Exception
+import qualified Data.Int as Int
 import qualified Endo
+import qualified System.Exit as Exit
+import qualified System.Mem as Mem
+
 
 main :: IO ()
-main = Endo.mainWith "test" ["--help"]
+main = do
+  ((), count) <- withAllocationCount
+    (withoutExit (Endo.mainWith "test" ["--help"]))
+  putStrLn ("allocated " <> show count <> " bytes")
+
+withoutExit :: IO () -> IO ()
+withoutExit = Exception.handle ignoreExit
+
+ignoreExit :: Exit.ExitCode -> IO ()
+ignoreExit _ = pure ()
+
+withAllocationCount :: IO a -> IO (a, Int.Int64)
+withAllocationCount action = do
+  before <- Mem.getAllocationCounter
+  result <- action
+  after <- Mem.getAllocationCounter
+  pure (result, before - after)
